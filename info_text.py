1b696081203202f18705f1f2851f0fb1971a25f6
LeDuble <rajarana92@gmail.com>
2023-05-29 01:37:05 +0300

    added info_text.py

---
+++ info_text.py
@@ -0,0 +1,28 @@
+def info_print():
+    print("""
+    ╭――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――╮
+    │                                                                                                  │
+    │  ██████  ██ ████████     ███████ ████████ ██  ██████  ██    ██ ███████ ████████ ████████ ███████ │
+    │ ██       ██    ██        ██         ██    ██ ██    ██ ██    ██ ██         ██       ██    ██      │
+    │ ██   ███ ██    ██        █████      ██    ██ ██    ██ ██    ██ █████      ██       ██    █████   │
+    │ ██    ██ ██    ██        ██         ██    ██ ██ ▄▄ ██ ██    ██ ██         ██       ██    ██      │
+    │  ██████  ██    ██        ███████    ██    ██  ██████   ██████  ███████    ██       ██    ███████ │
+    │                                                 ▀▀                                               │
+    │――――――――――――――――――――――――――――――――――――――――――――――――――――――Quick―Setup―――――――――――――――――――――――――――――――――│
+    │                                                                                                  │
+    │       Information:                                                                               │
+    │       Quick-Setup will make it just a breeze to get all the needed features, files and folders   │
+    │       included in the Git Etiquette.                                                             │
+    │                                                                                                  │
+    │       Also a great way to setup the .yaml workflow file to get automation going between issues   │
+    │       & projects.                                                                                │
+    │                                                                                                  │
+    │                                                                                                  │
+    │       Links:                                                                                     │
+    │       *- Documentation -*           https://github.com/LeDuble/Git-Etiquette                     │
+    │       *-   Template    -*           https://github.com/LeDuble/Git-Etiquette-Template            │
+    │       *-  Discussions  -*           https://github.com/LeDuble/Git-Etiquette/discussions         │
+    │                                                                                                  │
+    ╰――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――╯
+
+    """)
